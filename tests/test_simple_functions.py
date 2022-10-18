c7e759b9ee2433fc923d44fcb1749fcf3c826767
acse-c27c6d4a <levent.ugur22@imperial.ac.uk>
2022-10-18 13:15:17 +0100

    1

--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -1,6 +1,11 @@
 import pytest
 
+import numpy as np
+
+from constants import pi
+
 from simple_functions import my_sum
+from simple_functions import factorial
 
 
 class TestSimpleFunctions(object):
@@ -14,3 +19,23 @@ class TestSimpleFunctions(object):
         '''Test our add function'''
         isum = my_sum(iterable)
         assert isum == expected
+
+
+    @pytest.mark.parametrize('number, expected', [
+        (5, 120),
+        (3, 6),
+        (1, 1)
+    ])
+    def test_factorial(self, number, expected):
+        '''Test our factorial function'''
+        answer = factorial(number)
+        assert answer == expected
+
+
+class TestPi(object):
+    '''Class to test our constants are computed correctly'''
+
+    def test_pi(self):
+        '''Test computation of pi'''
+        my_pi = pi(2)
+        assert np.isclose(my_pi, np.pi, atol=1e-12)
